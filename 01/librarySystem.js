06955fb5ba85ac7142df1a49a3ba9d6c2a4910bb
Ugitch <ugison@gmail.com>
2019-05-26 11:46:25 +0200

    check dependencies

---
+++ 01/librarySystem.js
@@ -0,0 +1,44 @@
+// librarySystem; one global variable
+// 1. Create: librarySystem('libraryName', function() {/* return library */ });
+// 2. Use: librarySystem('libraryName')
+
+(function() {
+
+  var libraryStorage = {};
+  
+  function librarySystem(libraryName, dependencies, callback) {
+  //  check dependencies
+    if (dependencies && dependencies.length > 0) {
+      for (let i = 0 ; i < dependencies.length; i++) {
+        if (!(libraryStorage[dependencies[i]])) {
+          return;
+        }
+      }
+    }
+
+    // creating a library
+    if (arguments.length > 1) {
+      libraryStorage[libraryName] = callback();
+    // using library
+    } else {
+      return libraryStorage[libraryName];
+    }
+  }
+  // librarySystem function is available in global scope
+  window.librarySystem = librarySystem;
+
+})();
+
+librarySystem('name', [], function() {
+  return 'Gordon';
+});
+
+librarySystem('company', [], function() {
+  return 'Watch and Code';
+});
+
+librarySystem('workBlurb', ['name', 'company'], function(name, company) {
+  return name + ' works at ' + company;
+});
+
+librarySystem('workBlurb'); // 'Gordon works at Watch and Code'
