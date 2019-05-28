698363f88550d7fba1652d9a557500e1343b32c0
Ugitch <ugison@gmail.com>
2019-05-28 22:00:26 +0200

    comments

--- 01/librarySystem.js
+++ 01/librarySystem.js
@@ -8,21 +8,18 @@
   
   function librarySystem(libraryName, dependencies, callback) {
     // create library
-    // recursive case
     if (arguments.length > 1) {
       let dependencyModules = [];
       let dependencyLacking = [];
 
-      // if not an array, throw TypeError
       if (!Array.isArray(dependencies)) {
         throw new TypeError(`${dependencies} is not an array.`);
       }
-      // check if any dependencies
       else if (dependencies.length > 0){
         // arrange so dependencies are available to callback by their names
         dependencies.forEach(function(dependency) {
           if (dependency in libraryStorage) {
-            dependencyModules.push(librarySystem(dependency));
+            dependencyModules.push(librarySystem(dependency)); //recursive call
           }
           else {
             dependencyLacking.push(dependency);
@@ -30,19 +27,15 @@
         });
       }
 
-      // if all the dependencies are available, run the callback
       if (dependencies.length === dependencyModules.length) {
         libraryStorage[libraryName] = callback(...dependencyModules);
       }
-      // throw an error with information about missing dependencies
       else {
         throw new Error(`Library not loaded, lacks ${dependencyLacking.length} dependencies: ${dependencyLacking}.`);
       }
     
-    // base case
     // fetch library
     } else {
-      // throw an error if no library found
       if (!(libraryName in libraryStorage)) {
         throw new ReferenceError(`${libraryName} is not defined.`);
       }
@@ -50,7 +43,6 @@
       return libraryStorage[libraryName];
     }
   }
-  // librarySystem function is available in global scope
   root['librarySystem'] = librarySystem;
 
 })(this);
